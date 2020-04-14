596c85314485d55915c3e3247b04f969f26af49d
stella319 <jiashin319399@gmail.com>
2020-04-14 16:28:35 +0800

    var

--- ihealthnote/ViewController.swift
+++ ihealthnote/ViewController.swift
@@ -13,8 +13,36 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+        // 註釋
+    
+        // 變數
+        var index : Int = 1
+
+        // 常數
+        let opcode : Int = 2
+        
+        let result = index + opcode
+        
+        
+        print("result:\(result)")
+        
+        
+        if index == 2 {
+           print("2")
+        } else {
+            print("\(index )")
+        }
+   }
+
+
+    override func viewWillDisappear(_ animated: Bool) {
+        <#code#>
     }
 
-
+    
+    
+    
 }
+    
 
